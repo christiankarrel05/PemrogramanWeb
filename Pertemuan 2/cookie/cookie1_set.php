cea208c2d8e3f3df6b2cada23d5c85e59c1d45cd
christiankarrel05 <christiankarrel2015@gmail.com>
2022-11-09 12:24:36 +0700

    pertemuan5
    32132

---
+++ Pertemuan 2/cookie/cookie1_set.php
@@ -0,0 +1,11 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "angkasa";
+setcookie($cookie_name, $cookie_value, time()+3600, "/");
+
+if(isset($_COOKIE[$cookie_name])){
+    echo "cookie " . $cookie_name ." telah diatur!<br/>";
+    echo "nilainya adalah " . $_COOKIE[$cookie_name];
+} else {
+    echo "nama cookie ". $cookie_name." tidak diatur";
+}
---
+++ Pertemuan 2/latihan_auth/index.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+$database = [
+    'username' => 'christian',
+    'password' => 'kontol'
+];
+
+if(isset($_POST['submit'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username == $database['username'] && $password == $database['password']) {
+
+        $_SESSION['username'] = $username;
+
+        header("Location:admin.php");
+    } else {
+
+        echo '<script> alert("Username atau password salah!");
+        window.location="login-page.php"; </script>';
+    }
+} else {
+    echo '<script> window.location="login-page.php"; </script>';
+}
+?>
